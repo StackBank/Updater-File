f8d449e878ab7393f1b5fcfb33fff9fedb78c30c
Kiefe <56786730+ArtemSamarskiy@users.noreply.github.com>
2024-02-28 20:39:52 +0300

    Update update.lua

--- update.lua
+++ update.lua
@@ -8,13 +8,14 @@ cfg:setDefault('version', '0.0.0')
 local function try_update()
     local result_check = api:Post('check_update', { version = cfg:get('version'), token_check = data.token_check })
     if result_check and result_check.code == 200 and result_check.response.url_update then
-        cfg:set('version', result_check.response.lastVersion):save()
+        cfg:set('version', result_check.response.lastVersion)
+        cfg:save()
         shell.run('wget run ' .. result_check.response.url_update)
     end
 end
 
 while true do
-    local ws, err = http.websocket("wss://cc.kiefe.ru/updater/wss", { Authorization = data.token_check })
+    local ws, err = http.websocket("wss://updater.kiefe.ru/wss", { Authorization = data.token_check })
     if ws then
         try_update()
         while pcall(ws.receive) == 'New version!' do try_update() end
